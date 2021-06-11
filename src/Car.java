0b51e8fed13c13f72501bcbe6bc061104d18d648
Vladidlav <r_vlad@list.ru>
2021-06-11 07:32:53 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+     public Car(String name, int yearOfProdaction, int price, int weight, String color) {
+          this.name = name;
+          this.yearOfProdaction = yearOfProdaction;
+          this.price = price;
+          this.weight = weight;
+          this.color = color;
+     }
+
+     String name;
+     int yearOfProdaction;
+     int price;
+     int weight;
+     String color;
+
+
+          this.name = name;
+          this.yearOfProdaction = yearOfProdaction;
+          this.price = price;
+          this.weight = weight;
+          this.color = color;
+     }
+}
---
+++ src/CarManager.java
@@ -0,0 +1,13 @@
+public class CarManager {
+
+
+    public static void main(String[] args) {
+        Car prius = new Car("Toyota Prius",2000,15000,1200,"green");
+
+
+        Car renault = new Car("Renault Laguna", 2000, 9700,1600,"white");
+
+
+    }
+
+}
