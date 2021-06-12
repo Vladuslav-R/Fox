12214fa5a0428bf55ad15bf706a5bb1a5cc76b48
Vladidlav <r_vlad@list.ru>
2021-06-12 19:04:48 +0300

    1

--- src/Car.java
+++ src/Car.java
@@ -1,23 +1,89 @@
+import java.util.Objects;
+
 public class Car {
-     public Car(String name, int yearOfProdaction, int price, int weight, String color) {
-          this.name = name;
-          this.yearOfProdaction = yearOfProdaction;
-          this.price = price;
-          this.weight = weight;
-          this.color = color;
-     }
 
-     String name;
-     int yearOfProdaction;
-     int price;
-     int weight;
-     String color;
+    String name;
+    int yearOfProdaction;
+    int price;
+    int weight;
+    Color color;
+    private int distance = 0;
+    protected int distanceOnService = 0;
+
+    public Car(String name, int yearOfProdaction, int price, int weight, Color color) {
+        this.name = name;
+        this.yearOfProdaction = yearOfProdaction;
+        this.price = price;
+        this.weight = weight;
+        this.color = color;
+    }
 
 
-          this.name = name;
-          this.yearOfProdaction = yearOfProdaction;
-          this.price = price;
-          this.weight = weight;
-          this.color = color;
-     }
+
+    public void addDistance(int additionalDistance) {
+        distance += additionalDistance;
+        distanceOnService += additionalDistance;
+
+
+    }
+
+
+    public void addDistance(double additionalDistance) {
+        distance += additionalDistance;
+        distanceOnService += additionalDistance;
+
+    }
+
+    public void service() {
+        if (distanceOnService > 10000) {
+            distanceOnService = distanceOnService % 10000;
+        }
+    }
+
+    public boolean isReadyToSevice() {
+        if (distanceOnService > 10000) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int distanceOnService() {
+        return distanceOnService;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", yearOfProdaction=" + yearOfProdaction +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", color=" + color +
+                ", distance=" + distance +
+                ", distanceOnService=" + distanceOnService +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return yearOfProdaction == car.yearOfProdaction && price == car.price && weight == car.weight && Objects.equals(name, car.name) && color == car.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, yearOfProdaction, price, weight, color);
+    }
+
+
 }
+
--- src/CarManager.java
+++ src/CarManager.java
@@ -2,12 +2,28 @@ public class CarManager {
 
 
     public static void main(String[] args) {
-        Car prius = new Car("Toyota Prius",2000,15000,1200,"green");
+        PassangerCar prius = new PassangerCar("Toyota Prius", 2000, 15000, 1200, Color.GREEN);
 
 
-        Car renault = new Car("Renault Laguna", 2000, 9700,1600,"white");
+        Bus renault = new Bus("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 
 
+        Car renault2 = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
+        prius.addDistance(1000);
+        prius.addDistance(15000);
+        prius.addDistance(20.5);
+        renault.addDistance(15000);
+
+
+        System.out.println(prius);
+        System.out.println(renault);
+
+        System.out.println(renault);
+        System.out.println(prius.equals(renault));
+        System.out.println(renault.equals(renault2));
+        System.out.println(prius.isReadyToSevice());
+        System.out.println(renault.isReadyToSevice());
     }
+
 
 }
